106eb37c2309f2059ba2c39a3b5f8285f9e56477
shivani kate <shivani.n.kate@gmail.com>
2019-07-25 16:56:33 +0530

    Modified

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +9,7 @@ public class HelloWorldController {
 	
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
-		return "Hello Shivani Joshi";
+		return "Hello Shivani kate";
 	}
 	
 	@GetMapping(path="/hello-world-bean")
@@ -17,5 +17,9 @@ public class HelloWorldController {
 		return new helloWorldBean("Hello Shivani Kate");
 	}
 
+	@GetMapping(path="/hello-world-bean/path-variable/{name}")
+	public helloWorldBean helloWorldPathVariable(@PathVariable String name ) {
+		return new helloWorldBean(String.format("Hello Shivani Kate %s", name));
+	}
 
 }
